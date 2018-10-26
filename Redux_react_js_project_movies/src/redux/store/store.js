db9209943a49c3d77dedb9e9d626b94c7ee10249
Liliia Plonsak <plonsaklo@gmail.com>
2018-10-26 16:36:16 +0300

    added react_pattern_folder

--- Redux_react_js_project_movies/src/redux/store/store.js
+++ Redux_react_js_project_movies/src/redux/store/store.js
@@ -5,7 +5,9 @@ import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export const history = createBrowserHistory()
+
 const store = createStore(
     connectRouter(history)(rootReducer), 
     compose(applyMiddleware(thunk), DevTools));
